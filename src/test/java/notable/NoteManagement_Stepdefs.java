fa16884a7c5fb9a24e5df85dac7899fa975b581d
Jones Agyemang <mightyj@hotmail.co.uk>
2019-02-20 10:44:19 +0000

    Mock web response with Wiremock

--- src/test/java/notable/NoteManagement_Stepdefs.java
+++ src/test/java/notable/NoteManagement_Stepdefs.java
@@ -1,14 +1,48 @@
 package notable;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static junit.framework.TestCase.assertEquals;
 
 public class NoteManagement_Stepdefs {
+    private static final String NOTABLE_PATH = "/notable";
+    private WireMockServer wireMockServer = new WireMockServer();
+    private CloseableHttpClient httpClient = HttpClients.createDefault();
+
     @Given("^I am on the home page$")
-    public void i_am_on_the_home_page() {
-        executeGet("http://localhost:8080/notes");
+    public void i_am_on_the_home_page() throws IOException {
+        wireMockServer.start();
+
+        configureFor("localhost", 8080);
+        stubFor(get(urlEqualTo(NOTABLE_PATH)).willReturn(aResponse().withBody("Welcome to Notable!")));
+
+        HttpGet request = new HttpGet("http://localhost:8080/notable");
+        HttpResponse httpResponse = httpClient.execute(request);
+        String stringResponse = convertResponseToString(httpResponse);
+
+        verify(getRequestedFor(urlEqualTo(NOTABLE_PATH)));
+        assertEquals("Welcome to Notable!", stringResponse);
+    }
+
+    private static String convertResponseToString(@org.jetbrains.annotations.NotNull HttpResponse response) throws IOException {
+        InputStream responseStream = response.getEntity().getContent();
+        Scanner scanner = new Scanner(responseStream, "UTF-8");
+        String stringResponse = scanner.useDelimiter("\\Z").next();
+        scanner.close();
+        return stringResponse;
     }
 
     @When("^I enter my note$")
