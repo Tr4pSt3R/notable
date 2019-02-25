a0112f272bf36a03d3a6eb8ed8e815d22bc8b265
Jones Agyemang <mightyj@hotmail.co.uk>
2019-02-25 13:15:08 +0000

    Adds seed data for notes

--- src/main/java/notable/NotableApplication.java
+++ src/main/java/notable/NotableApplication.java
@@ -1,13 +1,29 @@
 package notable;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class NotableApplication {
 
+	@Autowired
+	NoteRepository noteRepository;;
+
 	public static void main(String[] args) {
 		SpringApplication.run(NotableApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			/* Add seed data */
+			noteRepository.save(new Note("1", "Lorem ipsum dolor"));
+			noteRepository.save(new Note("2", "Sriracha scenester cardigan"));
+			noteRepository.save(new Note("3", "Tofu commodo squid"));
+		};
 	}
 
 }
